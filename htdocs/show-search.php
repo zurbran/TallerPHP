9d257ebbf6b788f7aa8ff3aa585a6f7521d56085
Agustín Liébana <agus2009@gmail.com>
2018-04-23 20:12:26 -0300

    Puliendo estética

--- htdocs/show-search.php
+++ htdocs/show-search.php
@@ -10,11 +10,6 @@
 
     <link href="css/bootstrap.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-
-    <link href="css/single-book.css" rel="stylesheet">
-    <link href="css/media.css" rel="stylesheet">
-
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
 
@@ -25,6 +20,9 @@
         <?php
         require_once "pdo-connect.php";
         require_once 'paginator.class.php';
+
+        $searchA = $_GET['searchA'];
+        $searchT = $_GET['searchT'];
 
         $stmt= $pdo->prepare('SELECT l.autores_id, l.id, l.portada, l.titulo, a.nombre, a.apellido, l.cantidad FROM libros l INNER JOIN autores a ON (l.autores_id = a.id) WHERE (l.titulo LIKE :searchT) AND ((a.nombre LIKE :searchAn) OR (a.apellido LIKE :searchAa))');
 
@@ -46,20 +44,40 @@
 
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-6">
-            <img class="logo" src="img/logo-transparente.png" alt="logounlp">
+                <img class="logo" src="img/logo-transparente.png" alt="logounlp">
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-md-6">
+                <form action= 'show-search.php'>
+                    <fieldset>
+                    <legend>Refinar Búsqueda:</legend>
+                        <div class="form-group">
+                        <label for="titulobusqueda">Título</label>
+                        <input type="search" id="titulobusqueda" class="form-control" placeholder="Ingrese el título del libro a buscar" name="searchT" value="<?php echo htmlentities($searchT); ?>">
+                        </div>
+                        <div class="form-group">
+                        <label for="autorbusqueda">Autor</label>
+                        <input type="search" id="autorbusqueda" class="form-control" placeholder="Ingrese el autor el cual buscar" name="searchA" value="<?php echo htmlentities($searchA); ?>">
+                        </div>
+                        <div class="checkbox">
+                        </div>
+                        <button type="submit" name ="searchTt" value="Busqueda" class="btn btn-primary">Buscar</button>
+                    </fieldset>
+                </form>
             </div>
         </div>
 
         <hr/>
 
         <div class="row">
-            <?php echo "<p class='h1 titulo-libro'> Resultado de la búsqueda:</p>"?>
+            
         </div>
 
         <div class="row">
             <div class="col-md-1">
             </div>
             <div class="col-xs-16 col-sm-10 col-md-10" style="height: 600px; overflow: auto;">
+            <?php echo "<p class='h1 titulo-libro'> Resultado de la búsqueda:</p>"?>
                     <table class="table table-bordered">
                         <thead class="thead-dark">
                             <tr>
@@ -81,7 +99,6 @@
                             <th scope="row"><?php echo $Hinh ?></th>
                             <?php echo '<td><a href="/single-book.php?libro_id='.  $data[$i]["id"]  .'"> ' . $data[$i]["titulo"] .' </a></td> ';?>
                             <?php echo '<td><a href="/show-writers.php?author_id='. $data[$i]["autores_id"] .'&limit=5&page=1"> ' . $data[$i]["nombre"] . " " . $data[$i]["apellido"] . '</a></td>';?>
-                            
                             <td><?php echo $data[$i]["cantidad"] ?></td>
                             </tr>
                         <?php
