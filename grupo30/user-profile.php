c9e28dc53f1849ed9004c3ef6870512377b9d08d
Agustín Liébana <agustin.liebana@despegar.com>
2018-08-10 12:33:28 -0300

    vista de perfil

--- grupo30/user-profile.php
+++ grupo30/user-profile.php
@@ -1,19 +1,16 @@
 <?php
-    session_start();
-    require_once "user.class.php";
-    if(User::isLogged())
+session_start();
+require_once "user.class.php";
+if (User::isLogged()) {
+    try
     {
-        try
-        {
-            $user = User::login($_SESSION['email'],$_SESSION['password'],$pdo);
-        }
-        catch(Exception $e)
-        {
-            session_destroy();
-            $url = 'index.php?cred=false';
-            header( "Location: $url" );
-        }
-?>
+        $user = User::login($_SESSION['email'], $_SESSION['password'], $pdo);
+    } catch (Exception $e) {
+        session_destroy();
+        $url = 'index.php?cred=false';
+        header("Location: $url");
+    }
+    ?>
 <!DOCTYPE html>
 <html lang="es">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,9 +19,8 @@
     <title>Biblioteca UNLP - Perfil</title>
 
     <!-- Bootstrap Core CSS -->
-    <!-- <link href="css/bootstrap.min.css" rel="stylesheet"> -->
-
     <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/user-profile.css" rel="stylesheet">
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
@@ -33,34 +29,34 @@
     <script src="js/bootstrap.js"></script>
 
     <?php
-    require_once "pdo-connect.php";
+require_once "pdo-connect.php";
     require_once 'paginator.class.php';
 
     $pdoconn = $pdo;
 
-    $links= isset( $_GET['links'] ) ? $_GET['links'] : 10;
-    $limit= isset( $_GET['limit'] ) ? $_GET['limit'] : 5;
-    $page=  isset( $_GET['page'] )  ? $_GET['page']  : 1;
-    $sort=  isset( $_GET['sort'] )  ? $_GET['sort']  : 0;
-    $order= isset( $_GET['order'] ) ? $_GET['order'] : 0;
+    $links = isset($_GET['links']) ? $_GET['links'] : 10;
+    $limit = isset($_GET['limit']) ? $_GET['limit'] : 5;
+    $page = isset($_GET['page']) ? $_GET['page'] : 1;
+    $sort = isset($_GET['sort']) ? $_GET['sort'] : 0;
+    $order = isset($_GET['order']) ? $_GET['order'] : 0;
 
-    $query      = "SELECT l.id ,l.portada, l.titulo, l.autores_id, a.apellido, a.nombre, o.ultimo_estado, o.fecha_ultima_modificacion FROM usuarios u INNER JOIN operaciones o ON u.id=o.lector_id INNER JOIN libros l ON o.libros_id=l.id INNER JOIN autores a ON l.autores_id=a.id WHERE u.id=:userid";
+    $query = "SELECT l.id ,l.portada, l.titulo, l.autores_id, a.apellido, a.nombre, o.ultimo_estado, o.fecha_ultima_modificacion FROM usuarios u INNER JOIN operaciones o ON u.id=o.lector_id INNER JOIN libros l ON o.libros_id=l.id INNER JOIN autores a ON l.autores_id=a.id WHERE u.id=:userid";
 
-    $Paginator  = new Paginator( $pdoconn, $query, $sort, $order);
+    $Paginator = new Paginator($pdoconn, $query, $sort, $order);
 
-    $results    = $Paginator->getReaderHistory($limit, $page, $user->id);
+    $results = $Paginator->getReaderHistory($limit, $page, $user->id);
 
     ?>
-    
+
 </head>
 
 <body style="padding-top: 70px;">
 
     <div class="container-fluid fill-height">
         <div class="row">
-        <?php include "loggednavbar.php"; ?>
+        <?php include "loggednavbar.php";?>
         </div>
-        
+
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-6">
             <img class="logo" src="img/logo-transparente.png" alt="logounlp">
@@ -85,17 +81,18 @@
                 </div>
                 <div class="col-md-4">
                     <div class="row">
-                        <p> <?= $user->nombre?> </p>
+                        <p> <?=$user->nombre?> </p>
                     </div>
                     <div class="row">
-                        <p> <?= $user->apellido?> </p>
+                        <p> <?=$user->apellido?> </p>
                     </div>
                     <div class="row">
-                    <p> <?= $user->email?> </p>
+                    <p> <?=$user->email?> </p>
                     </div>
                 </div>
                 </div>
             </div>
+
             <div class="col-md-4">
                     <?php
                         $image_data = $user->foto;
@@ -103,18 +100,49 @@
                     ?>
                     <img  src="data:image/jpg;base64,<?=$encoded_image?>" width='200' height='200' />
             </div>
+
+            <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
         </div>
         <?php
-            if($user->isReader())
-            {
-                include "lector.php";
-            }
-        ?>    
+if ($user->isReader()) {
+        include "lector.php";
+    }
+    ?>
 
 </body>
 
 </html>
 
 <?php
-    }
-    ?>
+}
+?>
